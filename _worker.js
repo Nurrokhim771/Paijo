8effe3723dddeb5af4b0f9cfd8e7d8b2a4a6ea99
Nurrokhim771 <92316996+Nurrokhim771@users.noreply.github.com>
2025-07-16 13:07:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "sobepet493.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "paijo1"; // Ganti dengan nama workers kalian
+const serviceName = "paijo"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
